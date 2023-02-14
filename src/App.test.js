28a00711bf23059177dd37d5698b183f3e2f0262
naveen5765 <anaveen_7691@yahoo.com>
2023-02-14 11:21:00 +0100

    feat: should render title "Number to Word"

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test('should render title "Number to Word"', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const headerElement = screen.getByText(/Number to Word/i);
+
+  expect(headerElement).toBeInTheDocument();
 });
